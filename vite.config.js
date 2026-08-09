ba20a45cc06a10e522d403cb8d0fe7abb6753e64
vinodk07 <165988575+vinodk07@users.noreply.github.com>
2026-08-09 15:58:06 +0530

    Git Changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: '/portfolio/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 3000,
     open: true,
